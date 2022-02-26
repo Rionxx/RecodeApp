cd29683134fc0f549cee27184ac7867cd77637c4
Rion <rionfx.42@gmail.com>
2022-02-27 03:17:47 +0900

    first commit

---
+++ Recode/RecodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecodeApp.swift
+//  Recode
+//
+//  Created by FX on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct RecodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
